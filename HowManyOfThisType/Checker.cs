1c8e605428647d71d43fae758f151b8ea0614584
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-23 10:21:08 +0100

    Ex1 checker

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+    {
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            return 0;
+        }
+    }
+    
+}

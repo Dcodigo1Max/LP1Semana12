524a8cc22d5c9af96982272b6bbd3feec6c07e99
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-23 10:29:11 +0100

    Ex2 BeforeUtils

---
+++ TheyCameBefore/BeforeUtils.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheyCameBefore
+{
+    public static class BeforeUtils
+    {
+        public static GetTheOnesBefore()
+        {
+            
+        }
+    }
+}

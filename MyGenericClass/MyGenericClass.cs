3d6212641e022dfc148d99171473d4418abd90d6
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-23 10:35:41 +0100

    Generic Class

---
+++ MyGenericClass/MyGenericClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+        private T Start1;
+        private T Start2;
+        private T Start3;
+    }
+}
